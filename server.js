5914372354e7a52c7ce1b400f5a15fa290adca20
punkyfer <punkyfer666@gmail.com>
2021-02-16 23:07:06 +0100

    Changed server from express to http, added benchmark and profiling results

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 'use strict';
 
-const express = require('express');
-
+const http = require('http');
+const url = require('url');
 // Constants
 const PORT = 8080;
 const HOST = '0.0.0.0';
@@ -22,25 +22,7 @@ let publisher_campaigns = new Array(publisher_limit);
 var open_positions = [];
 var open_publishers = [];
 
-function find_max_campaign(position, publisher, res) {
-	if (position<=10000 && publisher<=10000) {
-		var all_campaigns = position_campaigns[position].concat(open_positions,publisher_campaigns[publisher],open_publishers);
-		var available_campaigns = all_campaigns.filter((e, i, a) => a.indexOf(e) !== i) // Get duplicates
-
-		var campaign = null;
-		var max_cpm = 0;
-		for (var i=0; i<available_campaigns.length; i++) {
-			if(campaign_prices[available_campaigns[i]]>max_cpm){
-				max_cpm = campaign_prices[available_campaigns[i]];
-				campaign = available_campaigns[i];
-			}
-		}
-		res.status(200).json({"price":max_cpm});
-	} else {
-		res.status(204).send();
-	}
-}
-
+// Load Redis data
 client.hvals("testcampaigns#", function(err, values){
 	// We can get away with using HVALS to extract the data instead of HSCAN (incremental iterations) since we only have 79 rows
 	for (var i=0; i<values.length; i++) {
@@ -71,12 +53,28 @@ client.hvals("testcampaigns#", function(err, values){
 	}
 });
 
-// App
-const app = express();
-app.get('/bid', (req, res) => {
-  find_max_campaign(req.query.position, req.query.publisherid, res);
-});
+function find_max_campaign(position, publisher, res) {
+	if (position<=10000 && publisher<=10000) {
+		var all_campaigns = [].concat(position_campaigns[position],open_positions,publisher_campaigns[publisher],open_publishers);
+		var available_campaigns = all_campaigns.filter((e, i, a) => a.indexOf(e) !== i) // Get duplicates
+		
+		var max_cpm = 0;
+		for (var i=0; i<available_campaigns.length; i++) {
+			if(campaign_prices[available_campaigns[i]]>max_cpm){
+				max_cpm = campaign_prices[available_campaigns[i]];
+			}
+		}
 
-app.listen(PORT, HOST);
-console.log(`Running on http://${HOST}:${PORT}`);
+		res.writeHead(200, {"Content-Type": "application/json"});
+		res.end(JSON.stringify({"price":max_cpm}));
+	} else {
+		res.writeHead(204, {'Connection': 'keepalive'});
+		res.end();
+	}
+}
 
+// create an http server to handle requests and response
+http.createServer(function (req, res) {
+	const queryObject = url.parse(req.url,true).query;
+	find_max_campaign(queryObject['position'], queryObject['publisherid'], res)
+}).listen(PORT);
